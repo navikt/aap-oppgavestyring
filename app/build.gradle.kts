3671ec2a2cbf056b164538b24b6fdadc81b72d49
Anders Østby <anders.ostby@nav.no>
2022-06-20 07:30:43 +0200

    Bumper avhengigheter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,14 +25,14 @@ dependencies {
     implementation("com.github.navikt.aap-libs:ktor-utils:2.0.2")
 
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.9.0")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.9.1")
     runtimeOnly("ch.qos.logback:logback-classic:1.2.11")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.2")
 
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("com.github.seratch:kotliquery:1.8.0")
-    implementation("org.flywaydb:flyway-core:8.5.12")
-    runtimeOnly("org.postgresql:postgresql:42.3.6")
+    implementation("org.flywaydb:flyway-core:8.5.13")
+    runtimeOnly("org.postgresql:postgresql:42.4.0")
 
     testImplementation(kotlin("test"))
     testImplementation("com.github.navikt.aap-libs:kafka-test:2.0.2")
