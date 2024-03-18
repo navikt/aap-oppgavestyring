0bed184918b922f9a0347daccb7b7d1ca4b990fb
Robin <robin.tordly@nav.no>
2024-03-18 15:03:48 +0100

    modules

---
+++ proxy/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("jvm") version "2.9.22"
+    id("io.ktor.plugin") version "3.3.9"
+}
+
+val aapLibVersion = "5.0.2"
+val ktorVersion = "2.3.9"
+
+application {
+    mainClass.set("proxy.OppgaveProxyKt")
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+
+    implementation("io.ktor:ktor-client-auth:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-client-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.12.4")
+    implementation("ch.qos.logback:logback-classic:1.5.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.0")
+
+    runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.4")
+}
+
+repositories {
+    mavenCentral()
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+}
+
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "21"
+    }
+    withType<Test> {
+        useJUnitPlatform()
+    }
+}
+
+kotlin.sourceSets["main"].kotlin.srcDirs("main")
+kotlin.sourceSets["test"].kotlin.srcDirs("test")
+sourceSets["main"].resources.srcDirs("main")
+sourceSets["test"].resources.srcDirs("test")
