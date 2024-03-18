d7cd58ea38569f0fcbf59620843bb1d169a874a4
Robin <robin.tordly@nav.no>
2024-03-18 14:54:05 +0100

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "org.example.AppKt"
+}
+
