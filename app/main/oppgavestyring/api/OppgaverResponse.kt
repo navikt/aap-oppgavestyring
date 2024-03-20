437204c8c603b71fb1e9fc50004e143394ebd4eb
Terje Heen <terje.heen@nav.no>
2024-03-20 12:43:15 +0100

    Utvider OppgaverResponse med felter matchende oppgavelisten i frontend-skisser

--- app/main/oppgavestyring/api/OppgaverResponse.kt
+++ app/main/oppgavestyring/api/OppgaverResponse.kt
@@ -1,9 +1,19 @@
 package oppgavestyring.api
 
-data class OppgavestyringResponse(
-    val oppgavetype: Oppgavetype
+import java.time.LocalDateTime
+
+data class OppgaverResponse(
+    val oppgaver: List<Oppgave>,
+)
+
+data class Oppgave(
+    val oppgaveId: Long,
+    val oppgavetype: Oppgavetype,
+    val foedselsnummer: String, //innbygger
+    val opprettet: String,
+    val reservertTil: String? = null
 )
 
 enum class Oppgavetype {
-
+    AVKLARINGSBEHOV
 }
