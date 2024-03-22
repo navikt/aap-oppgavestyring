9b5da39cdb7a6abb6ba7f99242332e2378f6e8d2
Robin <robin.tordly@nav.no>
2024-03-22 08:54:35 +0100

    hent alle eller ett kodeverk

--- docs/kodeverk.sh
+++ docs/kodeverk.sh
@@ -1,13 +1,35 @@
 #!/usr/local/bin/bash
 
+# USAGE:
+#   GET ONE
 #
-# Usage: ./kodeverk.sh <kodeverk>
+#     ./kodeverk.sh <kodeverk>
+#
+#   LIST ALL
+#
+#     ./kodeverk.sh
 #
 # Example ./kodeverk.sh Tema
 # Example ./kodeverk.sh Oppgavetyper
-#
 
-curl -X GET \
-    https://kodeverk.dev.intern.nav.no/api/v1/kodeverk/$1/koder \
-    -H 'Nav-Call-Id: aap' \
-    -H 'Nav-Consumer-Id: aap'
+lookup() {
+  curl -X GET \
+      https://kodeverk.dev.intern.nav.no/api/v1/kodeverk/$1/koder \
+      -H 'Nav-Call-Id: aap' \
+      -H 'Nav-Consumer-Id: aap'
+}
+
+
+all() {
+  curl -X 'GET' \
+    'https://kodeverk.dev.intern.nav.no/api/v1/kodeverk' \
+    -H 'accept: application/json' \
+    -H 'Nav-Consumer-Id: aap' \
+    -H 'Nav-Call-Id: aap'
+}
+
+if [ -z "$1" ]; then
+  all
+else
+  lookup $1
+fi
