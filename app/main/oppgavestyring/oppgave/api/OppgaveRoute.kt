5c07e8bc7ab04e4bb8b2f72120cc38c1342720ef
Terje Heen <terje.heen@nav.no>
2024-04-17 14:41:19 +0200

    Legger til logging av innkommende API-kall i route

--- app/main/oppgavestyring/oppgave/api/OppgaveRoute.kt
+++ app/main/oppgavestyring/oppgave/api/OppgaveRoute.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import oppgavestyring.LOG
 import oppgavestyring.authToken
 import oppgavestyring.oppgave.*
 import oppgavestyring.oppgave.adapter.*
@@ -16,6 +17,7 @@ fun Route.oppgaver(oppgaveRepository: OppgaveRepository, oppgaveClient: OppgaveC
     route("/oppgaver") {
 
         get {
+            LOG.info("Forsøker å søke opp alle oppgaver tilknyttet AAP")
 //            val token = call.authToken()
 //                ?: return@get call.respond(HttpStatusCode.Unauthorized)
 
@@ -29,6 +31,8 @@ fun Route.oppgaver(oppgaveRepository: OppgaveRepository, oppgaveClient: OppgaveC
         }
 
         get("/{id}") {
+            LOG.info("Forsøker å hente én konkret oppgave")
+
             val token = call.authToken()
                 ?: return@get call.respond(HttpStatusCode.Unauthorized)
 
@@ -46,6 +50,8 @@ fun Route.oppgaver(oppgaveRepository: OppgaveRepository, oppgaveClient: OppgaveC
         }
 
         post("/opprett") {
+            LOG.info("Forsøker å opprette en ny oppgave")
+
             val token = call.authToken()
                 ?: return@post call.respond(HttpStatusCode.Unauthorized)
 
@@ -60,6 +66,8 @@ fun Route.oppgaver(oppgaveRepository: OppgaveRepository, oppgaveClient: OppgaveC
         }
 
         patch("/{id}/tildelRessurs") {
+            LOG.info("Forsøker å tildele ressurs til oppgave")
+
             val token = call.authToken()
                 ?: return@patch call.respond(HttpStatusCode.Unauthorized)
             val id = call.parameters["id"]?.let { OppgaveId(it.toLong()) }
@@ -84,7 +92,7 @@ private fun map(it: SøkOppgaverResponse): OppgaverResponse {
                 oppgaveId = it.id,
                 versjon = it.versjon,
                 oppgavetype = Oppgavetype.AVKLARINGSBEHOV,
-                foedselsnummer = it.aktoerId!!, //TODO:
+                foedselsnummer = it.aktoerId!!, //TODO: Fødselsnummer, d-nummer eller aktørId?
                 tilordnetRessurs = it.tilordnetRessurs,
                 opprettet = it.opprettetTidspunkt!!
             )
