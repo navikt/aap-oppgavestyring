b3ba6d407b21c9e7e18c92fbe817fbae7498cd2d
Alexander Kjeserud <alexander.kjeserud@nav.no>
2024-04-29 21:27:53 +0200

    Removes url field from /behandling DTO

--- app/test/kotlin/oppgavestyring/oppgave/api/ApiTest.kt
+++ app/test/kotlin/oppgavestyring/oppgave/api/ApiTest.kt
@@ -9,32 +9,68 @@ import oppgavestyring.oppgave.adapter.Prioritet
 import oppgavestyring.oppgave.adapter.Status
 import oppgavestyring.oppgavestyringWithFakes
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.util.*
 
 class ApiTest {
 
-    @Test
-    fun `opprett oppgave`() {
-        oppgavestyringWithFakes { _, client ->
-            val actual = client.post("/behandling") {
-                contentType(ContentType.Application.Json)
-                bearerAuth("token")
-                accept(ContentType.Application.Json)
-                setBody(
-                    Request(
-                        saksnummer = "4LDQPDS",
-                        referanse = UUID.fromString("6ab21bd2-a036-40e5-bc14-cdc59b6ea4ce"),
-                        personident = "12345678911",
-                        avklaringsbehov = "Dette er et avklaringsbehov",
-                        url = "https://aap-saksbehandling.intern.dev.nav.no/sak/4LDQPDS/%7BbehandlingReferanse%7D",
-                        status = oppgavestyring.behandlingsflyt.Status.OPPRETTET
-                    )
-                )
-            }
+    @Nested
+    inner class `behandling`{
 
-            assertEquals(HttpStatusCode.Created, actual.status)
+        @Test
+        fun `opprett oppgave`() {
+            oppgavestyringWithFakes { _, client ->
+                val actual = client.post("/behandling") {
+                    contentType(ContentType.Application.Json)
+                    bearerAuth("token")
+                    accept(ContentType.Application.Json)
+                    setBody(
+                        Request(
+                            saksnummer = "4LDQPDS",
+                            referanse = UUID.fromString("6ab21bd2-a036-40e5-bc14-cdc59b6ea4ce"),
+                            personident = "12345678911",
+                            avklaringsbehov = "Dette er et avklaringsbehov",
+                            status = oppgavestyring.behandlingsflyt.Status.OPPRETTET
+                        )
+                    )
+                }
+
+                assertEquals(HttpStatusCode.Created, actual.status)
+            }
+        }
+
+        @Test
+        fun `opprett oppgave med ekstra properties`() {
+            data class TestRequest(
+                val referanse: UUID,
+                val saksnummer: String,
+                val personident: String,
+                val avklaringsbehov: String,
+                val someOtherProp: Boolean,
+                val status: oppgavestyring.behandlingsflyt.Status
+                )
+
+            oppgavestyringWithFakes { _, client ->
+                val actual = client.post("/behandling") {
+                    contentType(ContentType.Application.Json)
+                    bearerAuth("token")
+                    accept(ContentType.Application.Json)
+                    setBody(
+                        TestRequest(
+                            saksnummer = "23452345",
+                            personident = "12345678911",
+                            avklaringsbehov = "Dette er et avklaringsbehov",
+                            someOtherProp = false,
+                            status = oppgavestyring.behandlingsflyt.Status.OPPRETTET,
+                            referanse = UUID.randomUUID()
+                        )
+                    )
+                }
+
+                assertEquals(HttpStatusCode.Created, actual.status)
+            }
         }
     }
 
@@ -53,7 +89,6 @@ class ApiTest {
                         referanse = UUID.fromString("6ab21bd2-a036-40e5-bc14-cdc59b6ea4ce"),
                         personident = "12345678911",
                         avklaringsbehov = "Dette er et avklaringsbehov",
-                        url = "https://aap-saksbehandling.intern.dev.nav.no/sak/4LDQPDS/%7BbehandlingReferanse%7D",
                         status = oppgavestyring.behandlingsflyt.Status.OPPRETTET
                     )
                 )
