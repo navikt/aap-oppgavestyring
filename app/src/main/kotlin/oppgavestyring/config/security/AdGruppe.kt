47d16de829bf4ed00649163389363936cc1ab556
AlexKjes <alexander.kjeserud@nav.no>
2024-06-05 15:12:31 +0200

    [AAP-18] Legger til gruppestyring fra AD

--- app/src/main/kotlin/oppgavestyring/config/security/AdGruppe.kt
+++ app/src/main/kotlin/oppgavestyring/config/security/AdGruppe.kt
@@ -6,9 +6,9 @@ enum class AdGruppe(private val gruppeId: UUID) {
 
     SAKSBEHANDLER(UUID.fromString("8bb0ee13-49cd-4e75-8c3d-a13420c8b376")),
     VEILEDER(UUID.fromString("12353679-aa80-4e59-bb47-95e727bfe85c")),
-    AVDELINGSLEDER(UUID.randomUUID());
+    AVDELINGSLEDER(UUID.fromString("f0f6cad5-e3c0-4308-99a2-3630ac60174a"));
 
     companion object {
-        fun valueOf(gruppeId: UUID) = entries.find { gruppeId == it.gruppeId } ?: throw IllegalArgumentException("Gruppe $gruppeId not found")
+        fun valueOf(gruppeId: UUID) = entries.find { gruppeId == it.gruppeId }
     }
 }
--- app/src/main/kotlin/oppgavestyring/config/security/OppgavePrincipal.kt
+++ app/src/main/kotlin/oppgavestyring/config/security/OppgavePrincipal.kt
@@ -3,6 +3,7 @@ package oppgavestyring.config.security
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import oppgavestyring.oppgave.NavIdent
+import java.util.*
 
 data class OppgavePrincipal(
     val ident: NavIdent,
@@ -11,11 +12,11 @@ data class OppgavePrincipal(
     companion object {
         fun fromJwt(jwt: JWTCredential) = OppgavePrincipal(
             NavIdent(jwt.getClaim(NAV_IDENT_CLAIM_NAME, String::class) ?: throw IllegalArgumentException("JWT mangler Ident")),
-            AdGruppe.entries //AdGruppe.valueOf(jwt.getListClaim("groups", UUID::class))
+            jwt.getListClaim("groups", UUID::class).map { AdGruppe.valueOf(it) }.filterNotNull()
         )
     }
 
-    fun isVeileder() = AdGruppe.VEILEDER in grupper
-    fun isSaksbehandler() = AdGruppe.SAKSBEHANDLER in grupper
+    fun isVeileder() = AdGruppe.VEILEDER in grupper || AdGruppe.AVDELINGSLEDER in grupper
+    fun isSaksbehandler() = AdGruppe.SAKSBEHANDLER in grupper || AdGruppe.AVDELINGSLEDER in grupper
     fun isAvdelingsleder() = AdGruppe.AVDELINGSLEDER in grupper
 }
