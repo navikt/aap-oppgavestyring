8fdea326b288c943a1f8d49cff183f2960e59a7d
Fredrik Meyer <fredrik.meyer@nav.no>
2024-07-09 09:22:09 +0200

    Oppdater url til kodeverk-api i skript

--- docs/kodeverk.sh
+++ docs/kodeverk.sh
@@ -16,7 +16,7 @@
 
 lookup() {
   curl -X GET \
-      https://kodeverk.dev.intern.nav.no/api/v1/kodeverk/$1/koder \
+      https://kodeverk-api.nav.no/api/v1/kodeverk/$1/koder \
       -H 'Nav-Call-Id: aap' \
       -H 'Nav-Consumer-Id: aap'
 }
@@ -24,7 +24,7 @@ lookup() {
 
 all() {
   curl -X 'GET' \
-    'https://kodeverk.dev.intern.nav.no/api/v1/kodeverk' \
+    'https://kodeverk-api.nav.no/api/v1/kodeverk' \
     -H 'accept: application/json' \
     -H 'Nav-Consumer-Id: aap' \
     -H 'Nav-Call-Id: aap'
